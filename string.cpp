33de662169cbf7f43f0a8e39b8fc13b5362586a1
yuelinghou <76507035+yuelinghou@users.noreply.github.com>
2021-05-01 22:58:10 +0800

    string

---
+++ string.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "string.h"
+#include<string>
+#include <string>
+
+int main()
+{
+	my_string::test_string();
+	/*conventional::test_conventional();*/
+	/*int* p = new int(3);
+	delete[] p;*/
+	/*string s(NULL);*/
+	//cout << s.size() << endl;
+	//simulation_string::test_string();
+	/*string s1("hello");
+	s1.resize(0);
+	cout << s1.empty() << endl;
+	cout << s1.capacity() << endl;
+	cout << !5 << endl;*/
+	/*string s(nullptr);
+	cout <<s<< endl;*/
+	/*char* p = "abcdfg";*/
+	/*string s('a');*/
+	return 0;
+}
